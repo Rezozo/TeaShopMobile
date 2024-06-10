94af7039752439893990331ff0db5f79db2dc19f
Rezozo <110904181+Rezozo@users.noreply.github.com>
2024-06-10 19:19:27 +0300

    Create BaseUrlConfig.kt

---
+++ app/src/main/java/com/example/teashop/data/api/BaseUrlConfig.kt
@@ -0,0 +1,10 @@
+package com.example.teashop.data.api
+
+const val BASE_URL = "url"
+const val STRIPE_BASE_URL = "https://api.stripe.com"
+
+const val PUBLISH_KEY = "key"
+const val SECRET_KEY = "key"
+
+const val EMAIL = "email"
+const val EMAIL_PASSWORD = "password"
